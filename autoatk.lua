f379daee60b0da38e29a94bd035e1707ecb37003
dzfellon <82347541+dzfellon@users.noreply.github.com>
2021-04-11 21:40:42 -0300

    correção:Eduardo

--- autoatk.lua
+++ autoatk.lua
@@ -1,5 +1,3 @@
-local autoatk = TalkAction("!autoatk")
-
 local radiusX1 = 20
 local radiusY1 = 20
 local areapos = getCreaturePosition(cid)
@@ -11,41 +9,43 @@ function onTargetCreature(creature, target)
 return doChallengeCreature(creature, target)
 end
 ]]
+
+local autoatk = TalkAction("!autoatk")
 function autoatk.onSay(player, words, param)
 local split = param:split(",")
-	
-	local action = split[1]
+    
+    local action = split[1]
     if action == "face2face" then
-	player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Auto attack is activated on mode FACE2FACE!")
-		
-		if monster ~= nil
-		else if spectator => 1
-		then
-			local targets = monster:getTargetList()
-			local currentTarget = monster:getTarget()
-			if not currentTarget then
-				return monster:searchTarget()
-			end
-			if currentTarget ~= nil then
-				for i = 1, #targets do
-						local target = targets[i]
-				end
-				local newTarget = monster:getTargetList()[1]
-				monster:setTarget(newTarget)
-				monster:setFollowCreature(newTarget)
-			end
-		end
-		
-	-- MANTER DISTANCIA
-		
-	else if action == "distance3" then
-    doPlayerSendCancel(cid, "Auto attack is activated on mode DISTANCE Range: 3!")
-	
-	-- DESATIVAR
-	
-	else if action == "off" then
-    doPlayerSendCancel(cid, "Auto attack is OFF!")
-	
+    player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Auto attack is activated on mode FACE2FACE!")
+        
+        if monster ~= nil
+        else if spectator => 1
+        then
+            local targets = monster:getTargetList()
+            local currentTarget = monster:getTarget()
+            if not currentTarget then
+                return monster:searchTarget()
+            end
+            if currentTarget ~= nil then
+                for i = 1, #targets do
+                        local target = targets[i]
+                end
+                local newTarget = monster:getTargetList()[1]
+                monster:setTarget(newTarget)
+                monster:setFollowCreature(newTarget)
+            end
+        end
+        
+    -- MANTER DISTANCIA
+        
+    else if action == "distance3" then
+             player:sendCancelMessage("Auto attack is activated on mode DISTANCE Range: 3!")
+    
+    -- DESATIVAR
+    
+    else if action == "off" then
+             player:sendCancelMessage("Auto attack is OFF!")
+    
     return true
 end
 
